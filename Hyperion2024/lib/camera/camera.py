79b98e7abaa060886781f84e4d5dfdaf8b88d9c4
Sam Garg <132874866+SamGargRobotics@users.noreply.github.com>
2024-02-27 08:20:54 +1000

    CAMERA.PY
    IM CRAZY

---
+++ Hyperion2024/lib/camera/camera.py
@@ -0,0 +1,34 @@
+import sensor, image, time, pyb
+from pyb import UART
+
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QVGA)
+sensor.skip_frames(time = 2000)
+
+clock = time.clock()
+uart = UART(3, 115200, timeout_char = 10)
+thresholds = [(37, 66, -23, 127, -46, -24), (83, 95, -31, -18, -128, 93)] #blue first thresh, then yellow
+while(True):
+    clock.tick()
+    img = sensor.snapshot()
+    goalBlobs = img.find_blobs(thresholds, x_stride=5, y_stride=5, area_threshold=50, pixel_threshold=50, merge=False, margin=23)
+    blueBlobs = []
+    yellowBlobs = []
+    data = []
+    if goalBlobs:
+        largestBlob = goalBlobs[0]
+    for blob in goalBlobs:
+        if blob.code() == 1:
+            blueBlobs.append(blob)
+        elif blob.code() == 2:
+            yellowBlobs.append(blob)
+    for i in goalBlobs:
+        if i.area > largestBlob.area:
+            largestBlob = i
+    for i in data:
+        uart.writechar(i)
+    if isdebug:
+        #print(data)
+        img.draw_cross(int(img.width()/2), int(img.height()/2))
+
