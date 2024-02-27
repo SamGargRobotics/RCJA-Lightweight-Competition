bb0b01068c478a6ac48aad2e82882a5689773399
Sam Garg <132874866+SamGargRobotics@users.noreply.github.com>
2024-02-27 11:18:54 +1000

    Added Pins.h
    Added Pins master file to include drive and light system pins for ease

---
+++ Hyperion2024/lib/pins/pins.h
@@ -0,0 +1,6 @@
+#ifndef PINS_H
+#define PINS_H
+int tssp_Pin = 30;
+int light_Pin = A0;
+
+#endif
--- Hyperion2024/src/main.cpp
+++ Hyperion2024/src/main.cpp
@@ -2,23 +2,24 @@
 #include <tssp_system.h> //tssp system header file
 #include <drive_system.h> //motor system header file
 #include <camera.h> //camera header file
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BNO055.h>
-#include <imumaths.h>
+#include <Wire.h> //BNO File Header
+#include <Adafruit_Sensor.h> //BNO File Header
+#include <Adafruit_BNO055.h> //BNO File Header
+#include <imumaths.h> //BNO File Header
+#include <pins.h> //Pins master file
 
-Tssp_system tssp_sys = Tssp_system(30); //Creating Tssp System
-Light_System ls_sys = Light_System(A0); //Creating Light System
+Tssp_system tssp_sys = Tssp_system(tssp_Pin); //Creating Tssp System
+Light_System ls_sys = Light_System(light_Pin); //Creating Light System
 Drive_system drive_sys = Drive_system(); //Initializing Drive System
-bno::Adafruit_BNO055 compass;
-sensors_event_t direction;
+bno::Adafruit_BNO055 compass; //Initializing BNO
+sensors_event_t direction; //Initializing BNO
 
 void setup() {
   ls_sys.init(); //Initializing Light Sensors
   tssp_sys.init(); //Initializing Tssps
   drive_sys.init(); //Intializing Drive System
-  compass.setExtCrystalUse(true);
-  Serial.begin(9600);
+  compass.setExtCrystalUse(true); //Initializing BNO
+  Serial.begin(9600); //Intializing Serial
 }
 
 void loop() {
