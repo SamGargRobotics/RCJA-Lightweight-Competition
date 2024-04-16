f55f548b0a90e614dd619f16c5ddcaad175baca2
Samaksh Garg <23729@bbc.qld.edu.au>
2024-04-16 13:20:50 +1000

    Identified Error

--- Hyperion2024/lib/light system/light_system.cpp
+++ Hyperion2024/lib/light system/light_system.cpp
@@ -1,4 +1,5 @@
 #include "light_system.h"
+#include <common.h>
 
 void Light_System::init() {
     for (int i = 0; i < NUM_MUX; i++) {
@@ -20,3 +21,26 @@ void Light_System::mux(int muxPin) {
         muxPin = int(muxPin / 2);
     }
 }
+
+int Light_System::lineAvoidance() {
+    for (int i = 0; i < NUM_SENSORS; i++) {
+        if (lightData[i] >= line_avoidance_thresh) {
+            positiveData[i] = 1;
+        }
+    } for (int i = 0; i < NUM_SENSORS; i++) {
+        if (lightData[i] == 1) {
+            firstSens = i+1
+        }
+    } for (int i = 0; i < NUM_SENSORS; i++) {
+        if (lightData[firstSens+i] != 1) {
+            lastSens = firstSens+i;
+        }
+    }
+    if (lastSens < firstSens) {
+        difference = lastSens - firstSens;
+    } else {
+        difference = firstSens - lastSens;
+    }
+    movementAngle = (difference*22.5)-180;
+    return movementAngle;
+} //promblem 1: It will think sensor 1 is the first sensor instead of sensor 9 due to the if statement at line 34.
