faed06b30ad71532288c72633e597c246b451b5e
Samaksh Garg <23729@bbc.qld.edu.au>
2024-05-23 11:42:43 +1000

    Modified for showcase

--- Hyperion2024/src/main.cpp
+++ Hyperion2024/src/main.cpp
@@ -8,9 +8,6 @@
 #include <pins.h> //Pins master file
 #include <common.h> //Common master file
 
-
-
-
 Tssp_system tssp = Tssp_system(); // Creating IR ring System
 Light_System ls = Light_System(); //Creating Light System
 Drive_system motors = Drive_system(); //Creating Drive System
@@ -25,14 +22,14 @@ float ballDirection = 0;
 //Run through once (Arduino Setup)
 void setup()
 {
-  ls.init(); //Initializing Light Sensors
-  tssp.init(); //Initializing Tssps
+  //ls.init(); //Initializing Light Sensors
+  //tssp.init(); //Initializing Tssps
   motors.init(); //Intializing Drive System
-  compass.setExtCrystalUse(true); //Initializing BNO
+  //compass.setExtCrystalUse(true); //Initializing BNO
   Serial.begin(9600); //Intializing Serial
-  while(!compass.begin()) {
-    Serial.println("NOOOOOOOOOOOoo bno ded ;("); //Checking if BNO dies ;((
-  } 
+  //while(!compass.begin()) {
+  //  Serial.println("NOOOOOOOOOOOoo bno ded ;("); //Checking if BNO dies ;((
+  //}
 }
 
 //Run through continously (Arduino Loop)
@@ -52,23 +49,26 @@ void setup()
 //     //motors.run_all(orbit.calculate_Speed(tssp.tsspStrength), orbit.calculate_Direction2(ballDirection), compass_correct.update(compassVal, 0))
 //   }
 // }
-
+// ###### ACTUAL CODE:::::::
+// void loop() {
+//   compass.getEvent(&direction); //getting direction through BNo (direction.orientation.x)
+//   compassVal = direction.orientation.x;
+//   ballDirection = tssp.read();
+//   if (compassVal < CIRCLE_DEGREES && compassVal > SEMI_CIRCLE_DEGREES) {
+//     compassVal -= CIRCLE_DEGREES;
+//   }
+//   if (ls.lineAvoidance() > 0) {
+//     motors.run_all(100, ls.lineAvoidance(), 0);
+//   } else {
+//     if (compassVal <= 20 && compassVal >= -20) {
+//       motors.run_all(0, 0, compass_correct.update(compassVal, 0));
+//       //motors.run_all(100, orbit.calculate_Direction2(ballDirection), compass_correct.update(compassVal, 0))
+//     } else {
+//       motors.run_all(0, 0, compass_correct.update(compassVal, 0));
+//       //motors.run_all(orbit.calculate_Speed(tssp.tsspStrength), orbit.calculate_Direction2(ballDirection), compass_correct.update(compassVal, 0))
+//   }
+//   }
+// }
 void loop() {
-  compass.getEvent(&direction); //getting direction through BNo (direction.orientation.x)
-  compassVal = direction.orientation.x;
-  ballDirection = tssp.read();
-  if (compassVal < CIRCLE_DEGREES && compassVal > SEMI_CIRCLE_DEGREES) {
-    compassVal -= CIRCLE_DEGREES;
-  }
-  if (ls.lineAvoidance() > 0) {
-    motors.run_all(100, ls.lineAvoidance(), 0);
-  } else {
-    if (compassVal <= 20 && compassVal >= -20) {
-      motors.run_all(0, 0, compass_correct.update(compassVal, 0));
-      //motors.run_all(100, orbit.calculate_Direction2(ballDirection), compass_correct.update(compassVal, 0))
-    } else {
-      motors.run_all(0, 0, compass_correct.update(compassVal, 0));
-      //motors.run_all(orbit.calculate_Speed(tssp.tsspStrength), orbit.calculate_Direction2(ballDirection), compass_correct.update(compassVal, 0))
-  }
-  }
+  motors.run_all(20, 0, 0);
 }
